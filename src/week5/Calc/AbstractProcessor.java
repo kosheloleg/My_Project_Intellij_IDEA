7cc7414670f8a51bc645ade030d5e387abf0653a
Koshel Oleg <nkoleg@mail.ru>
2016-02-24 22:34:19 +0200

    calc

--- src/week5/Calc/AbstractProcessor.java
+++ src/week5/Calc/AbstractProcessor.java
@@ -3,14 +3,49 @@ package week5.Calc;
 public abstract  class AbstractProcessor implements Processorable {
     private int result;
     private int temp;
+    private char tempOperation;
     @Override
     public void printResult() {
-        System.out.println("Result = " + result);
+        System.out.println("Temp " + temp);
+        System.out.println("Result " + result);
+        System.out.println();
     }
+
+
     protected void setResult(int r){
         result = r;
     }
     public int getResult(){
         return result;
     }
+
+    public char getTempOperation() {
+        return tempOperation;
+    }
+
+    public void setTempOperation(char tempOperation) {
+
+        if (tempOperation=='='){
+
+            setResult(getResult()+getTemp());
+        }
+
+        else
+        this.tempOperation = tempOperation;
+    }
+
+    public void setTemp(int temp){
+
+        this.temp = temp;
+
+
+    }
+
+    public int getTemp(){
+
+        return temp;
+
+    }
+
+
 }
--- src/week5/Calc/TestProcessor.java
+++ src/week5/Calc/TestProcessor.java
@@ -1,11 +1,35 @@
 package week5.Calc;
 
-import week5.Calc.AbstractProcessor;
-
 public class TestProcessor extends AbstractProcessor {
 
     @Override
     public void inputChar(char c) {
         System.out.println("Введено "+c);
+
+        int digit = Character.getNumericValue(c);
+        String str="";
+
+        if (getTempOperation()!='\u0000' && getTemp()==getResult()){
+
+            super.setResult(0);
+
+        }
+
+        if (digit>=0){
+
+            str = ""+super.getResult()+c;
+
+
+            super.setResult(Integer.valueOf(str));
+
+        } else {
+
+            if (c!='=') super.setTemp(getResult());
+
+
+            super.setTempOperation(c);
+
+
+        }
     }
 }
